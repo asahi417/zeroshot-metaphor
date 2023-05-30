80a22b9a5f0cf41ee3c7eae0ded4ab0cda1f6fcc
asahi417 <asahi1992ushio@gmail.com>
2023-05-30 21:15:58 +0100

    sat

--- zeroshot_sat_metaphor.py
+++ zeroshot_sat_metaphor.py
@@ -32,10 +32,10 @@ language_models = {
     # "facebook/opt-iml-max-1.3b": [lmppl.LM, 4],  # 1.3B
     # "facebook/opt-iml-1.3b": [lmppl.LM, 4],  # 1.3B
     # "facebook/opt-30b": [lmppl.LM, 1],  # 30B
-    # "facebook/opt-13b": [lmppl.LM, 1],  # 1.3B
-    # "facebook/opt-1.3b": [lmppl.LM, 4],  # 1.3B
-    # "facebook/opt-350m": [lmppl.LM, 128],  # 350M
-    # "facebook/opt-125m": [lmppl.LM, 256],  # 125M
+    "facebook/opt-13b": [lmppl.LM, 1],  # 1.3B
+    "facebook/opt-1.3b": [lmppl.LM, 4],  # 1.3B
+    "facebook/opt-350m": [lmppl.LM, 128],  # 350M
+    "facebook/opt-125m": [lmppl.LM, 256],  # 125M
     # "EleutherAI/gpt-neox-20b": [lmppl.LM, 1],  # 20B
     # "EleutherAI/gpt-j-6B": [lmppl.LM, 4],  # 6B
     # "EleutherAI/gpt-neo-2.7B": [lmppl.LM, 8],  # 2.7B
@@ -45,22 +45,22 @@ language_models = {
     # "gpt2-large": [lmppl.LM, 128],  # 774M
     # "gpt2-medium": [lmppl.LM, 256],  # 355M
     # "gpt2": [lmppl.LM, 512],  # 124M
-    # "bert-large-cased": [lmppl.MaskedLM, 256],  # 355M
-    # "bert-base-cased": [lmppl.MaskedLM, 256],  # 110M
-    # "roberta-large": [lmppl.MaskedLM, 128],  # 355M
-    # "roberta-base": [lmppl.MaskedLM, 128],  # 110M
+    "bert-large-cased": [lmppl.MaskedLM, 256],  # 355M
+    "bert-base-cased": [lmppl.MaskedLM, 256],  # 110M
+    "roberta-large": [lmppl.MaskedLM, 128],  # 355M
+    "roberta-base": [lmppl.MaskedLM, 128],  # 110M
     # "google/ul2": [lmppl.EncoderDecoderLM, 1],  # 20B
     # "t5-11b": [lmppl.EncoderDecoderLM, 1],  # 11B
     # "t5-3b": [lmppl.EncoderDecoderLM, 4],  # 3B
     # "t5-large": [lmppl.EncoderDecoderLM, 128],  # 770M
     # "t5-base": [lmppl.EncoderDecoderLM, 512],  # 220M
     # "t5-small": [lmppl.EncoderDecoderLM, 512],  # 60M
-    "google/flan-ul2": [lmppl.EncoderDecoderLM, 1],  # 20B
-    "google/flan-t5-xxl": [lmppl.EncoderDecoderLM, 1],  # 11B
-    "google/flan-t5-xl": [lmppl.EncoderDecoderLM, 4],  # 3B
-    "google/flan-t5-large": [lmppl.EncoderDecoderLM, 128],  # 770M
-    "google/flan-t5-base": [lmppl.EncoderDecoderLM, 256],  # 220M
-    "google/flan-t5-small": [lmppl.EncoderDecoderLM, 256],  # 60M
+    # "google/flan-ul2": [lmppl.EncoderDecoderLM, 1],  # 20B
+    # "google/flan-t5-xxl": [lmppl.EncoderDecoderLM, 1],  # 11B
+    # "google/flan-t5-xl": [lmppl.EncoderDecoderLM, 4],  # 3B
+    # "google/flan-t5-large": [lmppl.EncoderDecoderLM, 128],  # 770M
+    # "google/flan-t5-base": [lmppl.EncoderDecoderLM, 256],  # 220M
+    # "google/flan-t5-small": [lmppl.EncoderDecoderLM, 256],  # 60M
     # "davinci": [lmppl.OpenAI, None]
 }
 
