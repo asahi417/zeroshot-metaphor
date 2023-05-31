5759a434416e4be127d0dfd09d1748ee246bee9a
asahi417 <asahi1992ushio@gmail.com>
2023-05-31 10:33:58 +0100

    sat

--- zeroshot_multi_choice_metaphor.py
+++ zeroshot_multi_choice_metaphor.py
@@ -26,6 +26,7 @@ language_models = {
     "facebook/opt-iml-max-1.3b": [lmppl.LM, 4],  # 1.3B
     "facebook/opt-iml-1.3b": [lmppl.LM, 4],  # 1.3B
     "facebook/opt-30b": [lmppl.LM, 1],  # 30B
+    "facebook/opt-13b": [lmppl.LM, 1],  # 1.3B
     "facebook/opt-1.3b": [lmppl.LM, 4],  # 1.3B
     "facebook/opt-350m": [lmppl.LM, 128],  # 350M
     "facebook/opt-125m": [lmppl.LM, 256],  # 125M
