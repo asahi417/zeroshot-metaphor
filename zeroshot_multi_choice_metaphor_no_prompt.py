bb546d99b738928c9e8c138a0d5ec72d81ce4222
asahi417 <asahi1992ushio@gmail.com>
2023-05-31 10:40:42 +0100

    sat

--- zeroshot_multi_choice_metaphor_no_prompt.py
+++ zeroshot_multi_choice_metaphor_no_prompt.py
@@ -114,7 +114,10 @@ if __name__ == '__main__':
             scores_file = f"metaphor_results/scores_no_prompt/{os.path.basename(target_model)}.{os.path.basename(target_data)}_{target_data_name}_{target_split}.json"
             if not os.path.exists(scores_file):
                 if scorer is None:
-                    scorer = lm_class(target_model, max_length=256) if lm_class is lmppl.MaskedLM else lm_class(target_model, device_map='auto', low_cpu_mem_usage=True)
+                    if lm_class is lmppl.MaskedLM:
+                        scorer = lm_class(target_model, max_length=256)
+                    else:
+                        scorer = lm_class(target_model, device_map='auto', low_cpu_mem_usage=True, offload_folder=f'offload_folder/{os.path.basename(target_model)}')
                 logging.info(f"[COMPUTING PERPLEXITY] model: `{target_model}`, data: `{target_data}/{target_data_name}/{target_split}`")
                 scores_dict = get_ppl(scorer, target_data, target_data_name, target_split, batch)
                 with open(scores_file, 'w') as f:
